054bbb24dcb69b5fd859846aad13b18db421d552
rbsie <rosie00r@gmail.com>
2025-12-26 09:44:51 +0100

    add comment

--- tsp_streamlit.py
+++ tsp_streamlit.py
@@ -31,7 +31,9 @@ def haversine(lat1, lon1, lat2, lon2):
     Calculates the haversine distance between two cities.
     """
     R = 6371  # Earth radius in km
+    # Convert degrees to radians
     dlat, dlon = radians(lat2 - lat1), radians(lon2 - lon1)
+    # Haversine formula
     a = sin(dlat/2)**2 + cos(radians(lat1))*cos(radians(lat2))*sin(dlon/2)**2
     return 2*R*atan2(sqrt(a), sqrt(1-a))
 
